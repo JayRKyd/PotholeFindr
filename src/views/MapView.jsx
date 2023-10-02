4b4a8f6a6f36af6473517768fa3670995bc39092
JayRKyd <knowlesjordy@gmail.com>
2023-10-02 14:45:54 -0400

    adding css changes

--- src/views/MapView.jsx
+++ src/views/MapView.jsx
@@ -10,22 +10,32 @@ const MapView = () => {
     console.log('Is User Signed In:', isSignedIn);
   return (
     <>
-<div className='flex flex-col items-center justify-center bg-slate-500 p-4'>
-  <div className='flex items-center mb-3'>
-    <img src={magnifyingGlass} alt="magnifying-glass" width={34} height={34} />
-    <h1 className='text-3xl font-bold ml-1 tracking-tight text-white'>PotholeFinder</h1>
-  </div>
-  <div className='flex flex-col gap-2 w-full flex-center-column'>
-    {/* Login Button */}
-    <Link to='/signin' className="flex max-w-fit items-center justify-center rounded-lg border border-gray-600 text-white px-5 py-2 text-sm shadow-md hover:bg-gray-400 bg-gray-600 font-medium transition">Login</Link>
-    
-    {/* Signup Button */}
-    <Link to='/signup' className="flex max-w-fit items-center justify-center rounded-lg border border-gray-600 text-white px-5 py-2 text-sm shadow-md hover:bg-gray-400 bg-gray-600 font-medium transition">Sign Up</Link>
-    
-    {/* Report Button */}
-    <Link to='/report' className="flex max-w-fit items-center justify-center rounded-lg border border-blue-600 text-white px-5 py-2 text-sm shadow-md hover:bg-blue-400 bg-blue-600 font-medium transition">Report a Pothole</Link>
-  </div>
-</div>
+
+      <div className='responsive flex justify-between items center bg-slate-500'>
+      <div className='flex items-center mb-3 ml-3'>
+      <img src={magnifyingGlass} alt="magnifying-glass" width={34} height={34} />
+      <h1 className='sm:text-3xl text-xl font-bold ml-1 tracking-tight text-white'>PotholeFinder</h1>
+      </div>
+      
+      <div className='test'>
+      <div className=' flex justify-end mt-3 border-b pb-7 sm:px-4 px-2 border-gray-500 gap-4 w-full'>
+      {isSignedIn ? (
+        <>
+        <SignedInHeader />
+        <UserButton />
+        <Link to='/signin' className="flex max-w-fit items-center justify-center space-x-2 rounded-lg border border-gray-600 text-white px-5 py-2 text-sm shadow-md hover:bg-gray-400 bg-gray-600 font-medium transition">Login</Link>
+          <Link to='/signup' className="">SignUp</Link>
+        </>
+      ) : (
+        <>
+          <Link to='/signin' className="flex max-w-fit items-center justify-center space-x-2 rounded-lg border border-gray-600 text-white px-5 py-2 text-sm shadow-md hover:bg-gray-400 bg-gray-600 font-medium transition">Login</Link>
+          <Link to='/signup' className="flex max-w-fit items-center justify-center space-x-2 rounded-lg border border-gray-600 text-white px-5 py-2 text-sm shadow-md hover:bg-gray-400 bg-gray-600 font-medium transition">SignUp</Link>
+        </>
+      )}
+      <Link to='/report' className="flex max-w-fit items-center justify-center space-x-2 rounded-lg border border-blue-600 text-white px-5 py-2 text-sm shadow-md hover:bg-blue-400 bg-blue-600 font-medium transition">Report a Pothole</Link>
+    </div>
+        </div>
+        </div>
           <MapComponent />
 
     </>
