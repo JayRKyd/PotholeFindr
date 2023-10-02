dd746305407e1212e341b44e207027a70e750680
JayRKyd <knowlesjordy@gmail.com>
2023-10-02 15:38:29 -0400

    fixing tailwind

--- src/views/ReportPothole.jsx
+++ src/views/ReportPothole.jsx
@@ -50,87 +50,86 @@ const ReportPothole = () => {
           Success! Thanks for your report!
         </p>
       ) : (
-        <form onSubmit={handleFormSubmit} className='bg-white flex flex-col rounded-lg w-1/2 p-4'>
-            <h2 className='text-3xl pb-4'>Report a Pothole</h2>
-            <div className='pb-4'>
-            <label htmlFor='severity' className='block text-lg pb-2 font-bold'>
-                Severity:
-                <input
-                type='text'
-                id='severity'
-                value={severity}
-                onChange={(e) => setSeverity(e.target.value)}
-                placeholder='Enter Severity'
-                required
-                className='block w-full mt-1 p-2 border border-gray-300 rounded-lg'
-                />
-            </label>
-            </div>
-            <div className='pb-4'>
-            <label htmlFor='description' className='block text-lg pb-2 font-bold'>
-                Description:
-                <textarea
-                id='description'
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-                required
-                className='block w-full mt-1 p-2 border border-gray-300 rounded-lg'
-                />
-            </label>
-            </div>
-            <div className='pb-4'>
-            <label htmlFor='latitude' className='block text-lg pb-2 font-bold'>
-                Latitude:
-                <input
-                type='text'
-                id='latitude'
-                value={latitude}
-                onChange={(e) => setLatitude(e.target.value)}
-                required
-                className='block w-full mt-1 p-2 border border-gray-300 rounded-lg'
-                />
-            </label>
-            </div>
-            <div className='pb-4'>
-            <label htmlFor='longitude' className='block text-lg pb-2 font-bold'>
-                Longitude:
-                <input
-                type='text'
-                id='longitude'
-                value={longitude}
-                onChange={(e) => setLongitude(e.target.value)}
-                required
-                className='block w-full mt-1 p-2 border border-gray-300 rounded-lg'
-                />
-            </label>
-            </div>
-            <div className='pb-4'>
-      <label htmlFor='constituency' className='block text-lg pb-2 font-bold'>
-        Constituency:
-        <select
-          id='constituency'
-          value={constituency}
-          onChange={(e) => setConstituency(e.target.value)}
-          required
-          className='block w-full mt-1 p-2 border border-gray-300 rounded-lg'
-        >
-          <option value='' disabled>
-            Select a Constituency
-          </option>
-          <option value='Constituency 1'>East Grand Bahama</option>
-          <option value='Constituency 2'>Marco City</option>
-          {/* Add more options as needed */}
-        </select>
-      </label>
-    </div>
-        
-            <button
-            type='submit'
-            className='bg-blue-500 text-white py-2 px-4 rounded-lg font-bold hover:bg-blue-700 transition'
-            >
-            Submit
-            </button>
-        </form>
+        <form onSubmit={handleFormSubmit} className='bg-white flex flex-col rounded-lg p-4 sm:w-1/2 mx-auto'>
+  <h2 className='text-3xl pb-4'>Report a Pothole</h2>
+  <div className='pb-4'>
+    <label htmlFor='severity' className='block text-lg pb-2 font-bold'>
+      Severity:
+      <input
+        type='text'
+        id='severity'
+        value={severity}
+        onChange={(e) => setSeverity(e.target.value)}
+        placeholder='Enter Severity'
+        required
+        className='block w-full mt-1 p-2 border border-gray-300 rounded-lg'
+      />
+    </label>
+  </div>
+  <div className='pb-4'>
+    <label htmlFor='description' className='block text-lg pb-2 font-bold'>
+      Description:
+      <textarea
+        id='description'
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        required
+        className='block w-full mt-1 p-2 border border-gray-300 rounded-lg'
+      />
+    </label>
+  </div>
+  <div className='pb-4'>
+    <label htmlFor='latitude' className='block text-lg pb-2 font-bold'>
+      Latitude:
+      <input
+        type='text'
+        id='latitude'
+        value={latitude}
+        onChange={(e) => setLatitude(e.target.value)}
+        required
+        className='block w-full mt-1 p-2 border border-gray-300 rounded-lg'
+      />
+    </label>
+  </div>
+  <div className='pb-4'>
+    <label htmlFor='longitude' className='block text-lg pb-2 font-bold'>
+      Longitude:
+      <input
+        type='text'
+        id='longitude'
+        value={longitude}
+        onChange={(e) => setLongitude(e.target.value)}
+        required
+        className='block w-full mt-1 p-2 border border-gray-300 rounded-lg'
+      />
+    </label>
+  </div>
+  <div className='pb-4'>
+    <label htmlFor='constituency' className='block text-lg pb-2 font-bold'>
+      Constituency:
+      <select
+        id='constituency'
+        value={constituency}
+        onChange={(e) => setConstituency(e.target.value)}
+        required
+        className='block w-full mt-1 p-2 border border-gray-300 rounded-lg'
+      >
+        <option value='' disabled>
+          Select a Constituency
+        </option>
+        <option value='Constituency 1'>East Grand Bahama</option>
+        <option value='Constituency 2'>Marco City</option>
+        {/* Add more options as needed */}
+      </select>
+    </label>
+  </div>
+  <button
+    type='submit'
+    className='bg-blue-500 text-white py-2 px-4 rounded-lg font-bold hover:bg-blue-700 transition w-full sm:w-auto'
+  >
+    Submit
+  </button>
+</form>
       )};
         </div>
 </>
